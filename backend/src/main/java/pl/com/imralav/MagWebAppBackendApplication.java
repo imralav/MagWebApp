d641ef9f25c1936bc9388b91c11112f4523e18a7
Tomasz Karczewski <t.karczewski@tt.com.pl>
2017-04-02 16:56:39 +0200

    Initial commit. Started backend and frontend projects

---
+++ backend/src/main/java/pl/com/imralav/MagWebAppBackendApplication.java
@@ -0,0 +1,12 @@
+package pl.com.imralav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagWebAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagWebAppBackendApplication.class, args);
+	}
+}
