69d810308249d05a73b7a3192c78c61aa7f3aa42
Tomasz Karczewski <karczewskit@tt.com.pl>
2017-05-06 18:01:39 +0200

    Navigation between pages is now working

--- frontend/app/components/navbar/navbar.js
+++ frontend/app/components/navbar/navbar.js
@@ -14,22 +14,38 @@
             bindToController: {
                 sectionName: '@',
                 leftIcon: '@',
-                rightIcon: '@'
+                leftRedirect: '@',
+                rightIcon: '@',
+                rightRedirect: '@'
             },
             templateUrl: 'navbar/navbar.html'
         };
     }
 
-    Controller.$inject = ['$translate'];
-    function Controller($translate) {
+    Controller.$inject = ['$translate', '$location'];
+    function Controller($translate, $location) {
         var vm = this;
         vm.toggleLanguage = toggleLanguage;
+        vm.leftRedirectTo = leftRedirectTo;
+        vm.rightRedirectTo = rightRedirectTo;
 
         function toggleLanguage() {
             if($translate.use() === 'pl') {
                 $translate.use('en');
             } else {
                 $translate.use('pl');
+            }
+        }
+
+        function leftRedirectTo() {
+            if(angular.isDefined(vm.leftRedirect)) {
+                $location.path(vm.leftRedirect);
+            }
+        }
+
+        function rightRedirectTo() {
+            if(angular.isDefined(vm.rightRedirect)) {
+                $location.path(vm.rightRedirect);
             }
         }
     }
